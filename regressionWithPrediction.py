d73d005a3a893262294038a4d8ae14f90961ef5d
chhavip <coolaqua12@gmail.com>
2016-12-16 21:39:08 +0530

    Added prediction and graph visualisations

---
+++ regressionWithPrediction.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import quandl, datetime
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+df = quandl.get('WIKI/GOOGL')
+
+#print(df.head())
+
+df = df[['Adj. Open','Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+
+# print(df.head())
+
+forecast_col = 'Adj. Close'
+df.fillna(-9999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+#print(df.tail())
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+
+
+X = X[:-forecast_out]
+X_late = X[-forecast_out:]
+
+
+df.dropna(inplace = True)
+
+#X = X[:-forecast_out+1]
+y = np.array(df['label'])
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+
+
+print('Confidence using Linear Regression')
+clf = LinearRegression()
+
+clf.fit(X_train, y_train)
+
+accu = clf.score(X_test, y_test)
+
+# print(accu)
+
+# print('Confidence using SVM in different modes')
+
+# for k in ['linear','poly','rbf','sigmoid']:
+#     clf = svm.SVR(kernel=k)
+#     clf.fit(X_train, y_train)
+#     confidence = clf.score(X_test, y_test)
+#     print(k,confidence)
+
+# print('Best comes out to be Linear Regression in this case')
+
+forecast_set = clf.predict(X_late)
+
+print(forecast_set, accu, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_da = 86400
+next_unix = last_unix + one_da
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_da
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+
+
+
