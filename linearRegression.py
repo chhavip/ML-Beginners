28e1a22bb0885fd3bde07a526fcc471634ea9226
chhavip <coolaqua12@gmail.com>
2016-12-16 23:17:39 +0530

    Linear Regression from scratch

---
+++ linearRegression.py
@@ -0,0 +1,36 @@
+from statistics import mean
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+xs = np.array([1,2,3,4,5,6], dtype = np.float64)
+zs = np.array([5,4,6,5,6,7], dtype = np.float64)
+
+# code to make the axes visible on a graph if needed
+# plt.plot(xs, zs)
+# plt.show()
+
+
+def best_fit_slope_and_intercept(xs, zs):
+	m = ( ((mean(xs) * mean (zs)) - mean(xs*zs)) /
+		 ((mean(xs)**2) - mean(xs**2)))
+	b = mean(zs) - m*mean(xs)
+	return m, b
+
+
+m,b = best_fit_slope_and_intercept(xs, zs)
+
+
+regression_line = [(m*x) + b for x in xs]
+print(m, b)
+
+#simple prediction
+predict_x = 8
+predict_z = (m*predict_x)+b
+
+plt.scatter(xs,zs)
+plt.scatter(predict_x, predict_z, color='g')
+plt.plot(xs, regression_line)
+plt.show()
