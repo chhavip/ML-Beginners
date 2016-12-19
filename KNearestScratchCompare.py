1915ce250627c935169120306dd4e1c68afcf634
chhavip <coolaqua12@gmail.com>
2016-12-19 19:52:48 +0530

    Comparison of scratch KNN on Breast Cancer Data

--- KNearestScratchCompare.py
+++ KNearestScratchCompare.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import warnings
 from collections import Counter
 import pandas as pd 
-import radom
+import random
 style.use('fivethirtyeight')
 
 #dataset = {'k' : [[1,2],[2,3],[3,1]],'r':[[6,5],[7,7],[8,6]]}
@@ -38,6 +38,39 @@ def k_nearest_neighbors(data, predict, k=3):
 	return vote_result
 
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
+df = pd.read_csv('breast-cancer-wisconsin.txt')
 
-print(result)
+df.replace('?', -99999, inplace =True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
+
+#print(full_data[:10])
+
+random.shuffle(full_data)
+
+test_size = 0.2
+
+train_set = {2:[], 4:[]}
+
+test_set = {2:[], 4:[]}
+
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+	train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+	test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+	for data in test_set[group]:
+		vote = k_nearest_neighbors(train_set, data, k=5)
+		if group == vote:
+			correct += 1
+		total += 1
+
+print('Accuracy:', correct/total)
